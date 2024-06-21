db65b670d965d6f6f7204912a7b6473773af579c
Derickdev2 <derickmedina132@gmail.com>
2024-06-20 20:24:15 -0400

    add task #9

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ *  main - This program prints the number 1-100, followed by a newline.
+ *  But for multiples of 3, prints 'Fizz' instead of the number,
+ *  for multiples of 5, prints 'Buzz', nd for multiples of both,
+ *  prints 'Fizzbuzz'
+ *  Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		for (i = 1; i <= 100; i++)
+		{
+			if (i % 15 == 0)
+				printf("FizzBuzz");
+			else if (i % 3 == 0)
+				printf("Fizz");
+			else if (i % 5 == 0)
+				printf("Buzz");
+			else
+				printf("%i", i);
+
+			if (i != 100)
+				printf(" ");
+
+		}
+
+		printf("\n");
+		return (0);
+	}
