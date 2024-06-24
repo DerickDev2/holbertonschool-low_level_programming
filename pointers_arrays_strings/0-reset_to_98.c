3f9846eb07420464d4c6df9350d01b5cac090939
Derickdev2 <derickmedina132@gmail.com>
2024-06-24 10:11:08 -0400

    add task #0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * reset_to_98 - a function that takes a pointer to an int a s parameter
- * and updates the value it points to 98
- * @n: input
- * return: n
+ * reset_to_98 - a function that takes a pointer to an int as parameter and updates
+ * the value it points to
+ * to 98
+ * @n: number to be processe
  */
 void reset_to_98(int *n)
 {
-	*n - 98;
+	*n = 98;
 }
