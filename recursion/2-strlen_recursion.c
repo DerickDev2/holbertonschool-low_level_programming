e11629127e7561c9979baa3845211a229da8daa9
Derickdev2 <derickmedina132@gmail.com>
2024-07-01 11:02:48 -0400

    add task #2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,11 +5,11 @@
  * @s:char arg
  * Return: int.
  */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		return (1 + _strlen_recursions(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 		return (0);
