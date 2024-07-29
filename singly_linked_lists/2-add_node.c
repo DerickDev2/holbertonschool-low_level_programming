aa67e7399b3d1877a760539aa1b24f6197886487
DerickDev2 <derickmedina132@gmail.com>
2024-07-29 13:14:53 -0400

    adds a new node at the begginning of a list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 #include <string.h>
 /**
-** add_node - adds a new node to start of a list
+**add_node - adds a new node to start of a list
 *@head: a pointer to pointer
 *@str: points to a contant char
 *Return: address of a new element of null
@@ -12,12 +12,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	/*variables*/
 	list_t *new_head = (list_t *)malloc(sizeof(list_t));
+
 	/*check str if empty*/
 	if (str == NULL || new_head == NULL)
 		return (NULL);
 	new_head->str = strdup(str);
 	new_head->len = strlen(str);
-		new_head->next = *head;
+	new_head->next = *head;
 	*head = new_head;
 
 	if (new_head->str == NULL)
